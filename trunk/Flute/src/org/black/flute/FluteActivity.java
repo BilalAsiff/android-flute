1266350ace33134a0bacb0df29dd9b1fb4dbe1b3
mrblack.tw@gmail.com <mrblack.tw@gmail.com@2bcdf8c2-70c9-e247-a544-299d56fb342e>
2011-08-03 15:20:11 +0000

    1. Provide the simple way to solve audio input's thread problem.

--- trunk/Flute/src/org/black/flute/FluteActivity.java
+++ trunk/Flute/src/org/black/flute/FluteActivity.java
@@ -1,18 +1,20 @@
 package org.black.flute;
 
+import java.lang.Thread.State;
+
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
 public class FluteActivity extends Activity {
 
-    RecordAudio audio = null;
+    private AudioInput audioInput = null;
+    private Thread audioInputThread = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,67 +28,118 @@ public class FluteActivity extends Activity {
         FluteSurfaceView fluteView = new FluteSurfaceView(this);
         FluteOnTouchListener fluteOnTouchListener = new FluteOnTouchListener();
         fluteView.setOnTouchListener(fluteOnTouchListener);
-        audio = new RecordAudio();
-        audio.execute();
-
         this.setContentView(fluteView);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        FluteGlobalValue.FLUTE_ON_WORKING = false;
-    }
-
-    @Override
-    protected void onRestart() {
-        // TODO Auto-generated method stub
-        super.onRestart();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         FluteGlobalValue.FLUTE_ON_WORKING = true;
+        Log.i(FluteConstant.APP_TAG,
+                "Assign FluteGlobalValue.FLUTE_ON_WORKING to true;");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (this.audioInputThread == null) {
+            this.audioInput = new AudioInput();
+
+            this.audioInputThread = new Thread(this.audioInput);
+            this.audioInputThread.start();
+        } else {
+            State threadState = this.audioInputThread.getState();
+            if (threadState.equals(State.TERMINATED) == true) {
+                try {
+                    this.audioInput = null;
+                    this.audioInput = new AudioInput();
+
+                    this.audioInputThread = null;
+                    this.audioInputThread = new Thread(this.audioInput);
+                    this.audioInputThread.start();
+                    System.gc();
+                } catch (Exception e) {
+                    Log.e(FluteConstant.APP_TAG, "Fail to re-launch thread", e);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FluteGlobalValue.FLUTE_ON_WORKING = false;
+        Log.d(FluteConstant.APP_TAG,
+                "Assign FluteGlobalValue.FLUTE_ON_WORKING to false");
     }
 
     @Override
     protected void onStop() {
-        // TODO Auto-generated method stub
         super.onStop();
+        if (this.audioInput != null) {
+            try {
+                audioInput.release();
+            } catch (Exception e) {
+                Log.e(FluteConstant.APP_TAG, "Release AudioInput Fail", e);
+            }
+        }
     }
 
-    private class RecordAudio extends AsyncTask<Void, double[], Void> {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
+    private class AudioInput implements Runnable {
         double max = -100;
         private final int sampleSize = 8196;
-        
+        private AudioRecord audioRecord = null;
+
+        public void release() {
+            if (this.audioRecord != null) {
+                try {
+                    this.audioRecord.release();
+                } catch (Exception e) {
+                    Log.e(FluteConstant.APP_TAG,
+                            "Fail to release AudioRelease!", e);
+                }
+            }
+        }
+
         @Override
-        protected Void doInBackground(Void... params) {
+        public void run() {
+            int sampleRateInHz = 44100;
+            int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_STEREO;
+            int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+            int audioSource = MediaRecorder.AudioSource.MIC;
+
             try {
-                int sampleRateInHz = 44100;
-                int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_STEREO;
-                int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
-                int audioSource = MediaRecorder.AudioSource.MIC;
                 int bufferSizeInBytes = AudioRecord.getMinBufferSize(
                         sampleRateInHz, channelConfig, audioFormat);
-                AudioRecord audioRecord = new AudioRecord(audioSource,
-                        sampleRateInHz, channelConfig, audioFormat,
-                        bufferSizeInBytes);
+                this.audioRecord = new AudioRecord(audioSource, sampleRateInHz,
+                        channelConfig, audioFormat, bufferSizeInBytes);
                 int audioRecordStatus = audioRecord.getState();
-                audioRecord.startRecording();
                 Log.d(FluteConstant.APP_TAG, "AudioRecord status: "
                         + audioRecordStatus);
-                short[] buffer = new short[this.sampleSize];
+                audioRecord.startRecording();
+                short[] audioData = new short[this.sampleSize];
                 double[] toTransform = new double[this.sampleSize];
                 while (FluteGlobalValue.FLUTE_ON_WORKING == true) {
-                    int bufferReadResult = audioRecord.read(buffer, 0, this.sampleSize);
+                    int bufferReadResult = audioRecord.read(audioData, 0,
+                            this.sampleSize);
                     for (int i = 0; i < this.sampleSize && i < bufferReadResult; i++) {
-                        // toTransform[i] = Math.abs((double) buffer[i]) /
-                        // 32767; // signed
+                        /*
+                         * Know the PCM-Decibel transform formula from the
+                         * following site:
+                         * http://stackoverflow.com/questions/2917762
+                         * /android-pcm-bytes But I am not sure this formula is
+                         * right or wrong.
+                         */
+
                         double f = 20 * Math.log10(toTransform[i]);
-                        if (buffer[i] > 0) {
-                            // Log.i("max", buffer[i] + "");
+                        if (audioData[i] > 0 && i == 4000) {
+                            Log.i(FluteConstant.APP_TAG, audioData[i] + "");
                             // Log.i("max", 20 * Math.log(0.9d) + "");
                         }
                         // Log.i("hello", 20 * Math.log10(0.1 / 32767) + "");
@@ -99,19 +152,27 @@ public class FluteActivity extends Activity {
                         // bit
                     }
 
-                    this.publishProgress(toTransform);
+                    FluteGlobalValue.getMotionEvent();
                 }
 
             } catch (Throwable t) {
                 Log.e(FluteConstant.APP_TAG, "Recording Failed", t);
             }
-
-            return null;
         }
 
-        protected void onProgressUpdate(double[]... toTransform) {
-            Log.i(FluteConstant.APP_TAG, "blow");
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            if (this.audioRecord != null) {
+                try {
+                    this.audioRecord.release();
+                } catch (Exception e) {
+                    Log.e(FluteConstant.APP_TAG, "Release AudioRecored Fail!",
+                            e);
+                }
+            }
         }
+
     }
 
 }
--- trunk/Flute/src/org/black/flute/FluteGlobalValue.java
+++ trunk/Flute/src/org/black/flute/FluteGlobalValue.java
@@ -1,5 +1,20 @@
 package org.black.flute;
 
+import android.view.MotionEvent;
+
 public class FluteGlobalValue {
     public static boolean FLUTE_ON_WORKING = false;
+    
+    private static MotionEvent motionEvent;
+
+    public synchronized static MotionEvent getMotionEvent() {
+        return motionEvent;
+    }
+
+    public synchronized static void setMotionEvent(MotionEvent motionEvent) {
+        FluteGlobalValue.motionEvent = null;
+        FluteGlobalValue.motionEvent = motionEvent;
+    }
+    
+    
 }
