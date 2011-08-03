a5c70b2645229f8b653f88ad765d27538431f7e7
mrblack.tw <mrblack.tw@gmail.com>
2011-08-03 15:20:11 +0000

    1. Provide the simple way to solve audio input's thread problem.

--- Flute/src/org/black/flute/FluteOnTouchListener.java
+++ Flute/src/org/black/flute/FluteOnTouchListener.java
@@ -6,17 +6,11 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 
 public class FluteOnTouchListener implements OnTouchListener {
-    private MotionEvent motionEvent;
-
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        // MotionEvent.ACTION_UP when gesture finish
         Log.i(FluteConstant.APP_TAG, "" + event.getPointerCount());
-        this.motionEvent = event;
+        FluteGlobalValue.setMotionEvent(event);
+        Log.i(FluteConstant.APP_TAG, "save");
         return true;
-    }
-
-    public MotionEvent getMotionEvent() {
-        return motionEvent;
     }
 }
